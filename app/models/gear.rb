006842a086ade0c85ced58d068e6515e921da103
Max Diener <mvdiener@gmail.com>
2015-01-15 13:06:09 -0800

    Added more fields for gear

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -1,5 +1,10 @@
 class Gear
   include Mongoid::Document
+
   field :category, type: String
   field :brand, type: String
+  field :year, type: Integer
+  field :name, type: String
+  field :attributes, type: Hash
+
 end
