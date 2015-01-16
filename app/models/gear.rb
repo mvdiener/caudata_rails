fdf821ca906768e421912a5758efd9c8507141bd
Max Diener <mvdiener@gmail.com>
2015-01-15 16:29:48 -0800

    Edited gear fields

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -1,10 +1,12 @@
 class Gear
   include Mongoid::Document
+  include Mongoid::Timestamps
 
   field :category, type: String
   field :brand, type: String
+  field :model, type: String
   field :year, type: Integer
+  field :specs, type: Hash
   field :name, type: String
-  field :attributes, type: Hash
 
 end
