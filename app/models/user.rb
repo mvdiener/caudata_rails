6ade574b4b8566fc35d330a5a33ae3b234b24834
Max Diener <mvdiener@gmail.com>
2015-01-18 13:18:59 -0800

    Added password hash check

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,25 +3,18 @@ require 'bcrypt'
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
-  include BCrypt
+  include ActiveModel::SecurePassword
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   field :first_name, type: String
   field :last_name, type: String
   field :email, type: String
-  field :password_hash, type: String
+  field :password_digest, type: String
 
   validates_format_of :email, with: VALID_EMAIL_REGEX
   validates_uniqueness_of :email
 
-  def password
-    @password ||= Password.new(password_hash)
-  end
-
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
-  end
+  has_secure_password
 
 end
