59f4e90d40e109781514b77211f5fb0a427b11ff
Max Diener <mvdiener@gmail.com>
2015-01-20 09:34:05 -0800

    Removed unnecessary method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,5 @@
 module SessionsHelper
 
-  # def current_user=(user)
-  #   @current_user = user
-  # end
-
   def current_user
     if session[:current_user_id]
       @current_user ||= User.find(session[:current_user_id])
