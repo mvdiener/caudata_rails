98c757f22e88f6e97578b5dd5c02a96b8a206155
Max Diener <mvdiener@gmail.com>
2015-01-27 15:18:12 -0800

    Working on validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,23 @@ class User
   include Mongoid::Timestamps
   include ActiveModel::SecurePassword
 
+  before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   field :first_name, type: String
   field :last_name, type: String
+  field :username, type: String
   field :email, type: String
   field :password_digest, type: String
 
-  before_save { self.email = email.downcase }
+  validates_presence_of :first_name, :last_name, :username, :password
 
   validates_format_of :email, with: VALID_EMAIL_REGEX
-  validates_uniqueness_of :email
+
+  validates :username, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: { case_sensitive: false }
 
   has_secure_password
+  validates_length_of :password, minimum: 8
 
 end
