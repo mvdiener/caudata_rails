f3730c276a6a01559f479661deb13ed84e766da9
Max Diener <mvdiener@gmail.com>
2015-01-27 16:06:13 -0800

    Working validations and error messages on sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,13 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    if user.save
+    if user.valid?
+      user.save
       session[:current_user_id] = user.id.to_s
       redirect_to root_path
     else
+      @errors = user.errors
+      @user = User.new
       render 'new'
     end
   end
@@ -21,7 +24,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:first_name, :last_name, :email, :username, :password, :password_confirmation)
     end
 
 end
