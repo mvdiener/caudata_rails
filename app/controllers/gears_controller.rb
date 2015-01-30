da823538901412159bc7734d8b182d2eb4e77d78
Max Diener <mvdiener@gmail.com>
2015-01-30 12:54:55 -0800

    Added view and controller for gear

---
+++ app/controllers/gears_controller.rb
@@ -0,0 +1,32 @@
+class GearsController < ApplicationController
+
+  def new
+    if current_user
+      @gear = Gear.new
+    else
+      redirect_to login_path
+    end
+  end
+
+  def create
+    current_user
+    gear = Gear.new(gear_params)
+    if gear.valid?
+      gear.save
+      @current_user.gears << gear
+      @current_user.save
+      redirect_to user_path(@current_user.id)
+    else
+      @errors = gear.errors
+      @gear = Gear.new
+      render 'new'
+    end
+  end
+
+  private
+
+    def gear_params
+      params.require(:gear).permit(:category, :brand, :model, :year, :spec, :name)
+    end
+
+end
