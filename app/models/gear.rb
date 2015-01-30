974416a8b6268ccf5855ada4a508563c7fa8e031
Max Diener <mvdiener@gmail.com>
2015-01-30 12:55:20 -0800

    Added a few validations for gear model

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -2,11 +2,15 @@ class Gear
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  belongs_to :user
+
   field :category, type: String
   field :brand, type: String
   field :model, type: String
   field :year, type: Integer
-  field :specs, type: Hash
+  field :spec, type: Hash
   field :name, type: String
+
+  validates_presence_of :category, :brand
 
 end
